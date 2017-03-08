861e180f8a4fd26b27f7aba8cbd91b99185fdf7f
dilguisonis <mslocox@gmail.com>
2017-03-07 21:04:26 -0300

    lol

---
+++ color/Assets/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void Move(float currentx)
+	{
+		//transform.transform.position = new Vector2 (50, transform.position.y);
+		//transform.position = new Vector2(currentx, transform.position.y);
+		transform.position=Vector2.Lerp (transform.position, new Vector2(currentx, transform.position.y), 5f);
+		//transform.position=Mathf.Lerp(transform.position, new Vector2(currentx, transform.position.y), Mathf.SmoothStep(0.0, 1.0, 5f));
+
+
+
+	}
+}
---
+++ color/Assets/Scripts/TouchDetection.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchDetection : MonoBehaviour {
+	public PlayerMovement PM;
+
+	public float maxTime;
+	public float minSwipeDist;
+
+	float startTime;
+	float endTime;
+
+	Vector3 startPos;
+	Vector3 endPos;
+	float swipeDistance;
+	float swipeTime;
+
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find ("Player");
+		PM = player.GetComponent<PlayerMovement> ();
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.touchCount > 0)
+		{
+			Touch touch = Input.GetTouch(0);
+			Debug.Log (touch.position.x);
+			if (touch.phase == TouchPhase.Moved) {
+				if (touch.position.x >= 0 && touch.position.x <= 200 ) {
+					if (player.gameObject.transform.position.x >= 0 || player.gameObject.transform.position.x <= 200) {
+
+						player.GetComponent<PlayerMovement> ().Move (touch.position.x);
+					
+					}
+				}
+			}
+
+			/*
+			if (touch.phase == TouchPhase.Began)
+			{
+				startTime = Time.time;
+				startPos = touch.position;
+
+				player.GetComponent<PlayerMovement> ().Move (touch.position.x);
+			}
+			else if (touch.phase == TouchPhase.Ended)
+			{
+				endTime = Time.time;
+				endPos = touch.position;
+
+				swipeDistance = (endPos - startPos).magnitude;
+				swipeTime = endTime - startTime;
+
+				if (swipeTime < maxTime && swipeDistance > minSwipeDist)
+				{
+					swipe();
+				}
+			}
+			*/
+		}
+	}
+
+	void swipe()
+	{
+		Vector2 distance = endPos - startPos;
+
+		if (Mathf.Abs(distance.x) > Mathf.Abs(distance.y))
+		{
+			Debug.Log("horizontal swipe");
+
+			/*
+			if (distance.x > 0)
+			{
+				Debug.Log("derecha");
+				player.GetComponent<PlayerMovement>().Derecha();
+			}
+			if (distance.x < 0)
+			{
+				player.GetComponent<PlayerMovement>().Izquierda();
+				Debug.Log("izquierda");
+			}
+			*/
+			//player.GetComponent<PlayerMovement>().Move();
+
+		}
+		else if (Mathf.Abs(distance.x) < Mathf.Abs(distance.y))
+		{
+			Debug.Log("vertical swipe");
+
+			if (distance.y > 0)
+			{
+				Debug.Log("arriba");
+
+//				player.GetComponent<PlayerMovement>().Jump();
+			}
+			if (distance.y < 0)
+			{
+				Debug.Log("abajo");
+			}
+		}
+	}
+}
